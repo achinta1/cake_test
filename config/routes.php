616e26971fe4857686423b22ba5c240742a32e45
Achinta Kundu <achinta.achinta@gmail.com>
2016-07-24 22:17:32 +0530

    Add files via upload

--- config/routes.php
+++ config/routes.php
@@ -41,7 +41,8 @@ use Cake\Routing\Router;
  *
  */
 Router::defaultRouteClass('DashedRoute');
-
+ // echo Router::url(['controller' => 'Articles', 'action' => 'view', 'id' => 15]); // Will output "/articles/15"
+ //echo Router::url(['_name' => 'login']);// Will output/login
 Router::scope('/', function (RouteBuilder $routes) {
     /**
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
@@ -60,13 +61,38 @@ Router::scope('/', function (RouteBuilder $routes) {
     
     
     
-    
     /**
      * Connect catchall routes for all controllers.
      *
      * Using the argument `DashedRoute`, the `fallbacks` method is a shortcut for
      *    `$routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'DashedRoute']);`
      *    `$routes->connect('/:controller/:action/*', [], ['routeClass' => 'DashedRoute']);`
+     *   $routes->connect('/:controller/\d+', [], ['routeClass' => 'DashedRoute']); \d+ regular expression so that only digits are matched
+     *
+     *  $routes->connect('/:controller/:id',['action' => 'view'],['id' => '[0-9]+']);
+     *
+     *  The DashedRoute class will make sure that the :controller and :plugin parameters are correctly lowercased and dashed.
+     *  $routes->connect('/:controller/:id',['action' => 'view'],['id' => '[0-9]+', 'routeClass' => 'DashedRoute']);
+     *
+     *
+     *  $routes->connect('/:controller/:year/:month/:day',['action' => 'index'],['year' => '[12][0-9]{3}','month' => '0[1-9]|1[012]','day' => '0[1-9]|[12][0-9]|3[01]']);
+     *
+     *
+     *  Passing Parameters to Action
+     *  $routes->connect('/blog/:id-:slug', // E.g. /blog/3-CakePHP_Rocks
+        ['controller' => 'Blogs', 'action' => 'view'],
+        [ // Define the route elements in the route template
+            // to pass as function arguments. Order matters since this
+            // will simply map ":id" to $articleId in your action
+            'pass' => ['id', 'slug'],
+            // Define a pattern that `id` must match.
+            'id' => '[0-9]+'
+        ]
+        );
+        //=======controller like ==========
+        public function view($articleId = null, $slug = null){// Some code here...}
+        
+     *  
      *
      * Any route class can be used with this method, such as:
      * - DashedRoute
@@ -79,6 +105,85 @@ Router::scope('/', function (RouteBuilder $routes) {
      */
     $routes->fallbacks('DashedRoute');
 });
+
+//=================pre fix===============
+/*
+ *Router::scope('/api', ['_namePrefix' => 'api:'], function ($routes) {
+    // This route's name will be `api:ping`
+    $routes->connect('/ping', ['controller' => 'Pings'], ['_name' => 'ping']);
+});
+ *
+ * Generate a URL for the ping route
+    Router::url(['_name' => 'api:ping']);
+
+    // Use namePrefix with plugin()
+    Router::plugin('Contacts', ['_namePrefix' => 'contacts:'], function ($routes) {
+    // Connect routes.
+    });
+
+    // Or with prefix()
+    Router::prefix('Admin', ['_namePrefix' => 'admin:'], function ($routes) {
+    // Connect routes.
+    });
+ *
+ *  Router::prefix('admin', ['param' => 'value'], function ($routes) {
+    // Routes connected here are prefixed with '/admin' and
+    // have the 'param' routing key set.
+    $routes->connect('/:controller');
+    });
+    
+    
+    //====================SEO-Friendly Routing ==================
+    Router::plugin('ToDo', ['path' => 'to-do'], function ($routes) {
+    $routes->fallbacks('DashedRoute');
+    });
+    
+    //==============Routing File Extensions================
+    Router::scope('/page', function ($routes) {
+    $routes->extensions(['json', 'xml', 'html']);
+    $routes->connect(
+        '/:title',
+        ['controller' => 'Pages', 'action' => 'view'],
+        [
+            'pass' => ['title']
+        ]
+    );
+    });
+    and on create must use
+    $this->Html->link('Link title',['controller' => 'Pages', 'action' => 'view', 'title' => 'super-article', '_ext' => 'html']);
+    
+    //==================Creating RESTful Routes=================
+    Router::scope('/', function ($routes) {
+    $routes->extensions(['json']);
+    $routes->resources('Recipes');
+    });
+    //**** foloow http://book.cakephp.org/3.0/en/development/routing.html
+    
+    //=========================Mapping Additional Resource Routes==============
+    $routes->resources('Articles', [
+        'map' => [
+            'deleteAll' => [
+                'action' => 'deleteAll',
+                'method' => 'DELETE'
+            ]
+        ]
+     ]);// This would connect /articles/deleteAll
+     
+     $routes->resources('Articles', [
+    'map' => [
+            'updateAll' => [
+                'action' => 'updateAll',
+                'method' => 'DELETE',
+                'path' => '/update_many'
+            ],
+        ]
+    ]);// This would connect /articles/update_many
+    
+    //===================Redirect Routing =====================
+    Router::scope('/', function ($routes) {
+        $routes->redirect('/articles/*', 'http://google.com', ['status' => 302]); //his would redirect /articles/* to http://google.com with a HTTP status of 302.
+    });
+*/
 
 /**
  * Load all plugin routes.  See the Plugin documentation on
